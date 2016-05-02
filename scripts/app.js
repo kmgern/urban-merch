fa70fe8ee1f3cc6177bdec0d7d25090ff368e73c
Kristina Gern <kmgern@gmail.com>
2016-05-02 17:42:50 +0100

    Add department page

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
+$('document').ready(function () {
+  $('[data-cart-add]').click(function () {
+    var cart = $('[data-cart-count]');
 
+    cart.html(Number(cart.text())+ 1);
+  });
+});
+
+$('document').ready(function () {
+  $('[data-cart-remove]').click(function () {
+    var cart = $('[data-cart-count]');
+    var cartCount = Number(cart.text());
+
+    if (cartCount > 0) {
+      cart.html(cartCount - 1);
+    }
+  });
+});
